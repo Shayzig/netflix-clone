02ba064f87b7c7f5617ba964024c1f16316da28b
Shay Zigdon <zigshay@gmail.com>
2024-01-03 11:36:13 +0200

    check deploy

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: './build', // Set the desired output directory
-    emptyOutDir: true,
-  },
+
   plugins: [react()],
   server: {
     port: 3000
